20ee0f90297f3450a95b2a6b4d0491c4f957263e
GZTimeWalker <gztimewalker@163.com>
2021-09-11 00:26:18 +0800

    Update Not Found Page

--- MSC.Server/ClientApp/src/pages/not-found/NotFoundPage.tsx
+++ MSC.Server/ClientApp/src/pages/not-found/NotFoundPage.tsx
@@ -4,7 +4,8 @@ import React, { FC } from 'react';
 export const NotFoundPage: FC = () => {
   return (
     <Center minHeight="100vh">
-      <Heading size="3xl">404 Not Found</Heading>
+      <Heading size="3xl">Oh，看起来你迷路了</Heading>
+      <Center>这里可不是你该来的地方</Center>
     </Center>
   );
 };
