92ef12259221f89839f21d7ffc8f82bfd432a39b
GZTimeWalker <gztimewalker@163.com>
2021-09-14 16:39:58 +0800

    Add ClientJS Column

---
+++ MSC.Server/Migrations/20210914083934_AddClientJS.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MSC.Server.Migrations
+{
+    public partial class AddClientJS : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClientJS",
+                table: "Puzzles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientJS",
+                table: "Puzzles");
+        }
+    }
+}
